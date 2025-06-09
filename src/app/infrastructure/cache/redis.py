ac26ace2439d8331f2bebc718f2094c67203a00d
Jorge Eduardo Vicente Hernández <jorge.eduardo@vicenteh.com>
2025-06-09 18:21:18 -0500

    Add files via upload
    Para uso de api publicada

--- src/app/infrastructure/cache/redis.py
+++ src/app/infrastructure/cache/redis.py
@@ -3,13 +3,29 @@ import json
 import os
 
 class RedisCache:
+
+# Para localhost
+#    def __init__(self):
+#        self.redis = redis.Redis(
+#            host=os.getenv("REDIS_HOST", "localhost"),
+#            port=int(os.getenv("REDIS_PORT", 6379)),
+#            db=int(os.getenv("REDIS_DB", 0)),
+#            decode_responses=True
+#        )
+
+# Para api publica
     def __init__(self):
-        self.redis = redis.Redis(
-            host=os.getenv("REDIS_HOST", "localhost"),
-            port=int(os.getenv("REDIS_PORT", 6379)),
-            db=int(os.getenv("REDIS_DB", 0)),
-            decode_responses=True
-        )
+        redis_url = os.getenv("REDIS_URL")
+
+        if redis_url:
+            self.redis = redis.from_url(redis_url, decode_responses=True)
+        else:
+            self.redis = redis.Redis(
+                host=os.getenv("REDIS_HOST", "localhost"),
+                port=int(os.getenv("REDIS_PORT", 6379)),
+                db=int(os.getenv("REDIS_DB", 0)),
+                decode_responses=True
+            )
 
     def get(self, key: str):
         data = self.redis.get(key)
