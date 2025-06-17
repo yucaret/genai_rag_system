11adeb9850275528327d7cac111dbc423b2b6806
Jorge Eduardo Vicente Hernández <jorge.eduardo@vicenteh.com>
2025-06-17 11:06:20 -0500

    Add files via upload

--- src/app/infrastructure/llm/rag_container.py
+++ src/app/infrastructure/llm/rag_container.py
@@ -1,4 +1,9 @@
 from src.app.infrastructure.llm.chains import RAGChain
 
+# Agrega: se crea una real carpeta para guardar el vector store, 17-06-2025
+os.makedirs("vector_db", exist_ok=True)
+
 # SINGLETON that will persist through app
-rag_chain = RAGChain(persistence_dir="vector_store")
+# Cambia: se cambia a la carpeta vector_db
+#rag_chain = RAGChain(persistence_dir="vector_store")
+rag_chain = RAGChain(persistence_dir="vector_db")
