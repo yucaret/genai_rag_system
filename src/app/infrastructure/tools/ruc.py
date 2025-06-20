e6d0df499c1dda7d2840148c7815658a71b7e555
Jorge Eduardo Vicente Hernández <jorge.eduardo@vicenteh.com>
2025-06-19 21:56:19 -0500

    Add files via upload

--- src/app/infrastructure/tools/ruc.py
+++ src/app/infrastructure/tools/ruc.py
@@ -1,1 +1,28 @@
-XX
+import os, requests
+from langchain.tools import tool
+
+API_URL   = "https://apiperu.dev/api/ruc_sunat"
+#token para colocarlo en redis
+API_TOKEN = os.getenv("APIPERU_TOKEN")
+
+@tool
+def consultar_datos_ruc(ruc: str) -> dict:
+    """Devuelve la ficha SUNAT de un número de RUC peruano."""
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}",
+        "Content-Type": "application/json",
+    }
+    try:
+        res = requests.post(API_URL, headers=headers, json={"ruc": ruc}, timeout=10)
+        res.raise_for_status()
+        data = res.json()
+
+        if not data.get("success"):
+            return {"error": data.get("message", "RUC inválido")}
+
+        return data["data"]
+
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Error HTTP: {str(e)}"}
+    except Exception as e:
+        return {"error": f"Error general: {str(e)}"}
