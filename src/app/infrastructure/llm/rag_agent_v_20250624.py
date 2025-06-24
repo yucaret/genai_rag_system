c9701d0848fa03b3eadd8fd384571a54ef786955
Jorge Eduardo Vicente Hernández <jorge.eduardo@vicenteh.com>
2025-06-24 18:19:48 -0500

    Add files via upload

--- src/app/infrastructure/llm/rag_agent_v_20250624.py
+++ src/app/infrastructure/llm/rag_agent_v_20250624.py
@@ -1,60 +1,33 @@
 from langchain.agents import initialize_agent, AgentType
 from src.app.infrastructure.tools import TOOLS
+from langchain.chat_models import ChatOpenAI
 from config.settings import settings
 from src.app.infrastructure.vector_store.faiss import FaissStore
 from src.app.infrastructure.llm.adapters import EmbeddingAdapter
 import numpy as np
 
-# Modificar 24-06-2025: Modificar librerias
-#from langchain.chat_models import ChatOpenAI
-from src.app.infrastructure.llm.providers import OpenAIProvider
-##
-
-# Agregar 24-06-2025: save_message
-from src.app.utils.memory import save_message
-##
-
 class RAGAgent:
     def __init__(self, vector_dir="vector_db"):
         self.embedder = EmbeddingAdapter()
         self.vector_store = FaissStore(self.embedder, persistence_dir=vector_dir)
-        
-        # Modificar 24-06-2025: Modificar llm
-        #self.llm = ChatOpenAI(
-	#        model=settings.openai_model,  # Ej: "gpt-3.5-turbo"
-	#        temperature=0,
-	#        openai_api_key=settings.openai_api_key
-        #)
-        self.llm = OpenAIProvider()
-        ##
-        
-        # Modificar 24-06-2025: Modificar initialize_agent
-        #self.agent = initialize_agent(
-        #    tools=TOOLS,
-        #    llm=self.llm,
-        #    agent=AgentType.OPENAI_FUNCTIONS,
-        #    verbose=True,
-        #)
+        self.llm = ChatOpenAI(
+	        model=settings.openai_model,  # Ej: "gpt-3.5-turbo"
+	        temperature=0,
+	        openai_api_key=settings.openai_api_key
+        )
+
         self.agent = initialize_agent(
-	    tools=TOOLS,
-	    llm=self.llm.llm,
-	    agent=AgentType.OPENAI_FUNCTIONS,
-	    verbose=True,
-	)
-	##
+            tools=TOOLS,
+            llm=self.llm,
+            agent=AgentType.OPENAI_FUNCTIONS,
+            verbose=True,
+        )
 
     def run(self, question: str) -> str:
         # 1) Pregunta directo al agente.  Si él necesita la vector-db, la tool, etc.
         try:
             answer = self.agent.run(question)
-            
-            # Agregar 24-06-2025: save_message user y assistant
-            save_message("user", question)
-            save_message("assistant", answer)
-            ##
-            
             return answer
-            
         except Exception:
             # 2) fallback manual a búsqueda vectorial (opcional)
             q_emb = np.array([self.embedder.get_embedding(question)], dtype=np.float32)
