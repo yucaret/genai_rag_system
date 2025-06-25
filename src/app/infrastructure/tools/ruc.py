c45f679bca79720c49ff7e1f0cf06d99a123cfac
Jorge Eduardo Vicente Hernández <jorge.eduardo@vicenteh.com>
2025-06-24 20:07:50 -0500

    Add files via upload

--- src/app/infrastructure/tools/ruc.py
+++ src/app/infrastructure/tools/ruc.py
@@ -7,7 +7,14 @@ API_TOKEN = os.getenv("APIPERU_TOKEN")
 
 @tool
 def consultar_datos_ruc(ruc: str) -> dict:
-    """Devuelve la ficha SUNAT de un número de RUC peruano."""
+    #"""Devuelve la ficha SUNAT de un número de RUC peruano."""
+    """
+    Usa esta herramienta cuando el usuario te proporcione un número de RUC peruano (11 digitos)
+    y te pida data, datos o información como nombre o razón social, estado, condición, dirección o datos
+    registrados en SUNAT. El número puede estar en medio del texto o al final, y no necesariamente
+    será acompañado de la palabra 'RUC', esta herramienta Devuelve la ficha SUNAT de un número de RUC
+    peruano.
+    """
     headers = {
         "Authorization": f"Bearer {API_TOKEN}",
         "Content-Type": "application/json",
